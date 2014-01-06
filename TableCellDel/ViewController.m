69c0d430dfba0967f3de83dd4313af82a4d0a85c
Chae Yungi <l8673901@gmail.com>
2014-01-06 16:24:27 +0900

    TableCellDel 01

--- TableCellDel/ViewController.m
+++ TableCellDel/ViewController.m
@@ -7,17 +7,39 @@
 //
 
 #import "ViewController.h"
+#define CELL_ID @"CELL_ID"
 
-@interface ViewController ()
+@interface ViewController () <UITableViewDelegate, UITableViewDataSource> {
+	NSMutableArray *data;
+}
 
 @end
 
 @implementation ViewController
 
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+	[data removeObjectAtIndex:indexPath.row];
+	
+	NSArray *rows = [NSArray arrayWithObject:indexPath];
+	[tableView deleteRowsAtIndexPaths:rows withRowAnimation:UITableViewRowAnimationAutomatic];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+	return [data count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+	UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CELL_ID];
+	
+	cell.textLabel.text = [data objectAtIndex:indexPath.row];
+	return cell;
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+	data = [NSMutableArray arrayWithObjects:@"A",@"B",@"C",@"D",@"E",@"F", nil];
 }
 
 - (void)didReceiveMemoryWarning
@@ -27,3 +49,27 @@
 }
 
 @end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
